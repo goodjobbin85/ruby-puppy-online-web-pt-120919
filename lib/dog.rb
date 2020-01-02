c706170d393583c26ee1cc3e85b4afaabaedf959
thomas dubiel <td1729@hotmail.com>
2020-01-02 04:04:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
-# Add your code here
+# Add your code here 
+
+class Dog 
+  attr_accessor :name 
+  
+  @@all = [] 
+  
+  def initialize(name) 
+    @name = name 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.clear_all 
+    while !@@all.empty?
+      @@all.each do 
+        @@all.pop
+      end 
+      @@all 
+    end
+  end 
+  
+  def self.print_all 
+    self.all.each do |dog|
+      puts dog.name
+    end
+  end
+  
+  def self.save
+    
+  end
+end
+
